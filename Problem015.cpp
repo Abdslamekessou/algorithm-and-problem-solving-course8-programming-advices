46638c9861d77222fd7b7672dc25a29594917dba
Abdessalem Kessouri <151072580+Abdslamekessou@users.noreply.github.com>
2025-10-19 14:47:02 +0100

    Create Problem015.cpp

---
+++ Problem015.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+/*
+🧠 Program to check if a given date is the last day of the month or the last day of the year.
+📅 Example:
+   Date = 31/12/2025
+📤 Output →
+   Yes, Day is Last Day in Month.
+   Yes, Day is Last Day in Year.
+*/
+
+
+using namespace std;
+
+
+short IsLeapYear(short year) {
+
+    return  (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? true : false;
+
+}
+
+
+struct sDate {
+
+    short Day;
+    short Month;
+    short Year;
+
+};
+
+
+short NumberOfDaysInAMonth(short month , short year) {
+
+    if (month < 1 || month > 12)
+        return 0;
+
+
+    int numberOfDays[12] = { 31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31  , 30 , 31 };
+
+    return (month == 2) ? (IsLeapYear(year) ? 29 : 28) : numberOfDays[month - 1]; // one line of condition
+
+}
+
+
+bool IsLastDayInMonth(sDate Date) {
+
+    return NumberOfDaysInAMonth(Date.Month, Date.Year) == Date.Day;
+
+}
+
+
+bool IsLastDayInYear(short Month) {
+
+    return Month == 12 ;
+
+}
+
+
+
+short ReadYear() {
+
+    short Year;
+
+    cout << "\nPlease enter a Year? ";
+    cin >> Year;
+
+    return Year;
+}
+
+
+short ReadMonth() {
+
+    short month;
+
+    cout << "\nPlease enter a Month ? ";
+    cin >> month;
+
+    return month;
+}
+
+
+
+short ReadDay() {
+
+    short day;
+
+    cout << "\nPlease enter a Day ? ";
+    cin >> day;
+
+    return day;
+}
+
+
+sDate ReadFullDate() {
+
+    sDate Date;
+
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
+
+
+
+int main()
+{
+    sDate Date = ReadFullDate();
+
+
+    if (IsLastDayInMonth(Date)) {
+
+        cout << "\nYes , Day is Last Day in Month.\n";
+
+    }
+    else {
+
+        cout << "\nYes , Day is NOT Last Day in Month.\n";
+
+    }
+
+
+    if (IsLastDayInYear(Date.Month)) {
+
+        cout << "\nYes , Day is Last Day in Year.\n";
+
+    }
+    else {
+
+        cout << "\nYes , Day is NOT Last Day in Year.\n";
+
+    }
+
+    system("pause>0");
+}
