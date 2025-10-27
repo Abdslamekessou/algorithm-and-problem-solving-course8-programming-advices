45f6f5c8347ae64f809debfb4af512c95c6c7b96
Abdessalem Kessouri <151072580+Abdslamekessou@users.noreply.github.com>
2025-10-27 06:37:57 +0100

    Add program to compare two dates

---
+++ Problem056.cpp
@@ -0,0 +1,119 @@
+
+#include <iostream>
+
+using namespace std;
+
+/*
+🧠 Program to compare two dates and determine if the first date is after the second date.
+
+📅 The program reads two full dates (day, month, year) from the user and compares them.
+
+🧩 Example:
+   Input →
+       Date1 : 1/1/2025
+       Date2 : 1/1/2000
+
+📤 Output →
+       Yes, Date1 is After Date2
+*/
+
+
+
+struct sDate {
+
+    short Day;
+    short Month;
+    short Year;
+
+};
+
+
+
+short ReadYear() {
+
+    short Year;
+
+    cout << "\nPlease enter a Year? ";
+    cin >> Year;
+
+    return Year;
+}
+
+
+short ReadMonth() {
+
+    short month;
+
+    cout << "\nPlease enter a Month ? ";
+    cin >> month;
+
+    return month;
+}
+
+
+
+short ReadDay() {
+
+    short day;
+
+    cout << "\nPlease enter a Day ? ";
+    cin >> day;
+
+    return day;
+}
+
+
+
+sDate ReadFullDate() {
+
+    sDate Date;
+
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
+
+
+bool IsDate1BeforeThanDate2(sDate Date1, sDate Date2) {
+
+    return (Date1.Year < Date2.Year) ? true : ((Date1.Year == Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month == Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+
+}
+
+
+bool IsDate1EqualsDate2(sDate Date1, sDate Date2) {
+
+    return (Date1.Year == Date2.Year ? (Date1.Month == Date2.Month ? (Date1.Day == Date2.Day ? true : false) : false) : false);
+}
+
+
+
+bool IsDate1AfterDate2(sDate Date1, sDate Date2) {
+
+    return (!IsDate1BeforeThanDate2(Date1, Date2) && !IsDate1EqualsDate2(Date1, Date2));
+
+}
+
+
+
+int main()
+{
+    
+    sDate Date1 = ReadFullDate();
+
+    sDate Date2 = ReadFullDate();
+
+    if (IsDate1AfterDate2(Date1, Date2)) {
+        cout << "\nYes , Date1 is After Date2";
+    }
+    else {
+        cout << "\nNo , Date1 is NOT After Date2";
+    }
+
+    system("pause>0");
+
+}
+
+
